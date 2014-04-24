d05f8e8a85dc67383ba6b0875d0c8f25aa5b2157
Thulio Philipe <thulioph@gmail.com>
2014-04-24 02:20:07 -0300

    Modificando o cabeçalho do retorno

--- proxy.php
+++ proxy.php
@@ -1,7 +1,7 @@
 <?php
- header('Content-type: text/html; charset=utf-8');
+ header('Content-type: application/json; charset=utf-8');
 
-$url = "http://api.tumblr.com/v2/blog/thulioph.tumblr.com/posts/text?api_key=oRPllhCbg5hkjgcDcsrG0FpmsaPhuDkt9ztQV0fGOKMWrnAIto";
+$url = "http://api.tumblr.com/v2/blog/blog.thulioph.com/posts/text?api_key=oRPllhCbg5hkjgcDcsrG0FpmsaPhuDkt9ztQV0fGOKMWrnAIto";
 $dados = file_get_contents($url);
 
 echo $dados;
