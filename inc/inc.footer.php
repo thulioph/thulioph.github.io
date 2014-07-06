72c4c435bde41f95eee10d0e9085eacc9fbf233f
Thulio Philipe <thulioph@gmail.com>
2014-07-05 23:54:57 -0300

    Excluindo scripts não utilizados e minificando o javascript para corrigir o erro de não abrir o feeds do blog

--- inc/inc.footer.php
+++ inc/inc.footer.php
@@ -2,8 +2,7 @@
         <!-- SCRIPTS -->
         <script src="js/vendor/jquery.min.js"></script>
         <script src="js/vendor/jquery.tooltipster.js"></script>
-        <script src="js/scripts.min.js"></script>
-        <!-- // <script src="js/cache.js"></script> -->
+        <script src="js/main.min.js"></script>
 
         <script>
             Main.setUp();
