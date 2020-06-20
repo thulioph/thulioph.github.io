35d263f4679aec4e6691ecf114ad6aef915b1c33
Thulio Philipe <thulioph@gmail.com>
2020-06-20 05:02:29 -0300

    feat(projects): update how projects are displayed

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,6 +3,7 @@ import React from 'react'
 import withGrid from './withGrid'
 import Loading from './loading'
 import { useProjects } from '../hooks/use-projects'
+import getTechIcon from './getTechIcon'
 
 const Projects = () => {
   const { projects } = useProjects()
@@ -10,42 +11,40 @@ const Projects = () => {
   return (
     <React.Fragment>
       <Loading when={!projects || !projects.length}>
-        {projects && projects.map(
-          ({ id, title, description, link, mainImage, techStack }) => (
-            <aside className="project-item" key={id}>
-              <h5 className="project-title">{title}</h5>
-              <a
-                href={link}
-                title={title}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                view site
-              </a>
+        {projects &&
+          projects.map(
+            ({ id, title, description, link, mainImage, techStack }) => (
+              <aside className="project-item" key={id}>
+                <h5 className="project-title">{title}</h5>
+                <a
+                  href={link}
+                  title={title}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  view site
+                </a>
 
-              <div className="project-image">
-                <figure>
-                  <img src={mainImage} alt={title} />
-                </figure>
-              </div>
+                <div className="project-image">
+                  <figure>
+                    <img src={mainImage} alt={title} />
+                  </figure>
+                </div>
 
-              <div className="stack-list">
-                <ul>
-                  {techStack.map((tech, idx) => (
-                    <li key={idx}>
-                      <img
-                        src={`/public/images/${tech}`}
-                        alt={tech}
-                      />
-                    </li>
-                  ))}
-                </ul>
+                <div className="stack-list">
+                  <ul>
+                    {techStack.map((tech, idx) => (
+                      <li key={idx}>
+                        <img src={getTechIcon(tech)} alt={tech} title={tech} />
+                      </li>
+                    ))}
+                  </ul>
 
-                <p>{description}</p>
-              </div>
-            </aside>
-          )
-        )}
+                  <p>{description}</p>
+                </div>
+              </aside>
+            )
+          )}
       </Loading>
     </React.Fragment>
   )
