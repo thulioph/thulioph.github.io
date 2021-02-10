c2f413d85ed0b721f5884e0d12386380011b3044
Thulio Philipe <thulioph@gmail.com>
2021-02-10 15:35:13 -0300

    fix(general): small and different changes

--- personal-info.js
+++ personal-info.js
@@ -1,6 +1,13 @@
 module.exports = {
-  title: "Thulio Philipe (thulioph)",
-  description: `I'm working as a developer for more than 8 years. I already worked with ad agencies, startups,software studios and also as a consultant, always focused on the Front-End side (UI). My current focus is on WebApps and some topics that guide my studies are Performance, Security, User Engagement and Code Quality I like to share my knowledge through <a href="https://speakerdeck.com/thulioph">talks</a>, <a href="https://medium.com/@thulioph_">articles</a> and all repositories on my <a href="https://github.com/thulioph">Github</a> are open. My current focus is on WebApps and some topics that guide my studies are Performance, Security, User Engagement and Code Quality.`,
+  title: "Thulio Philipe, Software Developer",
+  jobTitle: "Software Developer",
+  currentCompany: {
+    label: "ThoughtWorks",
+    website: "https://www.thoughtworks.com/"
+  },
+  description: `
+    I have been working as a software developer for almost 10 years. I already worked with ad agencies, startups,software studios and also as a consultant, always focused on the Front-End side (UI). My current focus is on WebApps and some topics that guide my studies are Performance, Security, User Engagement and Code Quality I like to share my knowledge through <a href="https://speakerdeck.com/thulioph">talks</a>, <a href="https://medium.com/@thulioph_">articles</a> and all repositories on my <a href="https://github.com/thulioph">Github</a> are open. My current focus is on WebApps and some topics that guide my studies are Performance, Security, User Engagement and Code Quality.
+  `,
   image:
     "https://avatars2.githubusercontent.com/u/2343288?s=460&u=34b95449c5414b41560dd5b41c97cf414bad8c8c&v=4",
   author: "thulioph",
