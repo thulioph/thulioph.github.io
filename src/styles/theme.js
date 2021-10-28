6fdb554f6c57fbf021455a38f831248adf625e15
Thulio Philipe <thulioph@gmail.com>
2021-10-28 16:56:26 -0300

    feat(theme): adds theme structure

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+export const light = {
+	colors: {
+		fonts: '#000000',
+		body: '#FFFFFF',
+		lines: '#EAEAEA'
+	}
+}
+
+export const dark = {
+	colors: {
+		fonts: '#FFFFFF',
+		body: '#000000',
+		lines: '#EAEAEA'
+	}
+}
