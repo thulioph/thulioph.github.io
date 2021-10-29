d30e8b4da497036cb0bf386b3962e9be68bab59e
Thulio Philipe <thulioph@gmail.com>
2021-10-29 01:30:12 -0300

    feat(header): adds a new header

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import { useSiteMetadata } from '../hooks/use-site-metadata'
-
-import { LogoIcon, TwitterIcon, LinkedinIcon, GithubIcon } from "./icons"
+import { LogoIcon } from "./icons"
 
 const Header = () => {
-  const { twitter, github, linkedin } = useSiteMetadata()
-
-  const menuLinks = [
-    { url: '/about', value: 'About' },
-    { url: '/#talks', value: 'Talks' },
-    { url: '/#projects', value: 'Projects' },
-    { url: '/writings', value: 'Writings' },
-  ]
-
-  const socialLinks = [
-    {
-      url: `https://twitter.com/${twitter}`,
-      value: "Twitter",
-      icon: <TwitterIcon width={30} height={30} />,
-    },
-    {
-      url: `https://linkedin.com/in/${linkedin}`,
-      value: "LinkedIn",
-      icon: <LinkedinIcon width={30} height={30} />,
-    },
-    {
-      url: `https://github.com/${github}`,
-      value: "Github",
-      icon: <GithubIcon width={30} height={30} />,
-    },
-  ]
-
   return (
     <React.Fragment>
       <header className="header">
@@ -45,30 +16,10 @@ const Header = () => {
 
           <menu className="menu">
             <ul className="menu-links">
-              {menuLinks.map(link => (
-                <li key={link.value}>
-                  <a href={link.url} title={link.value}>
-                    {link.value}
-                  </a>
-                </li>
-              ))}
+              <Link activeClassName="js-active" to="/about">About me</Link>
+              <Link activeClassName="js-active" to="/writings">Blog</Link>
             </ul>
           </menu>
-
-          <ul className="social-links">
-            {socialLinks.map(link => (
-              <li key={link.value}>
-                <a
-                  href={link.url}
-                  title={link.value}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  {link.icon}
-                </a>
-              </li>
-            ))}
-          </ul>
         </div>
       </header>
     </React.Fragment>
