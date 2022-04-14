8f55be5cfcb9564d8fffe5820d798adc91e71395
Thulio Philipe <thulioph@gmail.com>
2022-04-14 13:22:23 -0300

    Revert "feat: adds HelloFresh as current job (#31)"
    This reverts commit 7d96dc3cd059d5bf09494fffaa33c18d942a80da.

--- personal-info.js
+++ personal-info.js
@@ -9,8 +9,8 @@ module.exports = {
   headline: "building software and changing the world",
   title: "Thulio Philipe, Senior Software Developer",
   currentCompany: {
-    label: "HelloFresh",
-    website: "https://www.hellofresh.com/"
+    label: "ThoughtWorks",
+    website: "https://www.thoughtworks.com/"
   },
   description: `
     I have been working as a software developer for almost 10 years. I already worked with ad agencies, startups,software studios and also as a consultant, always focused on the Front-End side (UI). My current focus is on WebApps and some topics that guide my studies are Performance, Security, User Engagement and Code Quality I like to share my knowledge through <a href="https://speakerdeck.com/thulioph">talks</a>, <a href="https://medium.com/@thulioph_">articles</a> and all repositories on my <a href="https://github.com/thulioph">Github</a> are open. My current focus is on WebApps and some topics that guide my studies are Performance, Security, User Engagement and Code Quality.
@@ -45,13 +45,8 @@ module.exports = {
   },
   experience: [
     {
-      title: 'HelloFresh',
-      period: '2022 - current',
-      website: 'https://www.hellofresh.com/'
-    },
-    {
       title: 'Thoughtworks',
-      period: '2019 - 2021',
+      period: '2019 - current',
       website: 'https://www.thoughtworks.com/'
     },
     {
