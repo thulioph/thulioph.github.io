f59fabad4b8a55a1833a540542a1b35874b4067e
Thulio Philipe <thulioph@gmail.com>
2022-08-19 23:31:38 +0200

    fix(lint): moves things to the right place

--- components/header/index.js
+++ components/header/index.js
@@ -2,23 +2,16 @@ import React from "react";
 import Head from "next/head";
 
 const AppHeader = ({ children }) => {
+  const defaultTitle = "Thulio Philipe, Senior Software Engineer";
+
   return (
     <React.Fragment>
       <Head>
-        <title>{children} | Thulio Philipe, Senior Software Engineer</title>
-        <meta
-          name="description"
-          content={`${children} | Thulio Philipe, Senior Software Engineer`}
-        />
+        <title>
+          {children} | {defaultTitle}
+        </title>
+        <meta name="description" content={`${children} | ${defaultTitle}`} />
         <link rel="icon" href="/favicon.ico" />
-
-        {/* Google Web Font */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
     </React.Fragment>
   );
