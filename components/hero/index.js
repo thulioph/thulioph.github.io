84d2f9ff5695baf001728d214f5e594e4c881e85
Thulio Philipe <thulioph@gmail.com>
2023-01-21 21:30:51 +0100

    feat(component): adds hero component

---
+++ components/hero/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Image from "next/image";
+
+const Hero = ({ image, children, description }) => {
+  return (
+    <section className="hero">
+      <aside className="hero-image">
+        {image && (
+          <Image
+            src={image}
+            priority={true}
+            alt={children}
+            layout="responsive"
+          />
+        )}
+      </aside>
+
+      <aside className="hero-text">
+        <h1>{children}</h1>
+        <p>{description}</p>
+      </aside>
+    </section>
+  );
+};
+
+export default Hero;
