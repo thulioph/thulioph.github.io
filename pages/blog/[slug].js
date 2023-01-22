64a32a3a2f8d3786e68f5d154960a85327cdb223
Thulio Philipe <thulioph@gmail.com>
2023-01-22 01:38:09 +0100

    feat(component): updates more posts section

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,11 +1,11 @@
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 
 import AppHeader from "@/components/header";
 import AppNav from "@/components/navbar";
+import PostCard from "@/components/post-card";
 import { getPosts } from "@/services/index";
 
 const getImagePath = (image) => `/images/posts/${image}`;
@@ -100,15 +100,13 @@ const Post = ({ previousPost, currentPost, nextPost }) => {
 
           <aside className="more-posts-container">
             {previousPost && (
-              <Link href={previousPost?.slug} title={previousPost?.title}>
+              <PostCard slug={previousPost?.slug}>
                 {previousPost?.title}
-              </Link>
+              </PostCard>
             )}
 
             {nextPost && (
-              <Link href={nextPost?.slug} title={nextPost?.title}>
-                {nextPost?.title}
-              </Link>
+              <PostCard slug={nextPost?.slug}>{nextPost?.title}</PostCard>
             )}
           </aside>
         </section>
